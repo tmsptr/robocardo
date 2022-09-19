0999eb9f5ef065ea717e481f12f6de1d31437d44
Tomas Petruoka <petruokat@gmail.com>
2022-09-19 22:34:31 +0300

    Updates

---
+++ precache-manifest.80bf828a01557dcdb65aafb5be07f7b8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a12a7193006d83a445d9",
+    "url": "/robocardo/static/css/main.0efe4ac3.chunk.css"
+  },
+  {
+    "revision": "a12a7193006d83a445d9",
+    "url": "/robocardo/static/js/main.a12a7193.chunk.js"
+  },
+  {
+    "revision": "7f8c91ddc4b58ecb5b87",
+    "url": "/robocardo/static/js/1.7f8c91dd.chunk.js"
+  },
+  {
+    "revision": "bcfe793157f5de41918d",
+    "url": "/robocardo/static/css/2.7245f6f1.chunk.css"
+  },
+  {
+    "revision": "bcfe793157f5de41918d",
+    "url": "/robocardo/static/js/2.bcfe7931.chunk.js"
+  },
+  {
+    "revision": "3e7aeef165074f7d3830",
+    "url": "/robocardo/static/js/runtime~main.3e7aeef1.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robocardo/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "d3b5dbd9920bb91ce7c566124ad7b7d0",
+    "url": "/robocardo/index.html"
+  }
+];
